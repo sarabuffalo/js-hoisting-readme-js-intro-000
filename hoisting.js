954da860d86f6c592a2ac76788e61c595122c461
sarabuffalo <sep.14094@yahoo.com>
2018-05-06 12:35:51 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -1,23 +1,28 @@
 function callMe() {
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
+
 }
 
 
 function crazy() {
   // fix the code in here:
+var word = "hey!!!"
   thisIsCrazy();
 
   var thisIsCrazy = function (){
-    console.log("hey!!!")
+    console.log(word)
   }
 }
 
 function sayMyName() {
+  var name = "Kristin";
+  console.log("Kristin");
+}
   // fix the code in here:
   var name = "Cricky";
 
@@ -25,6 +30,5 @@ function sayMyName() {
 
   function sayMy() {
     console.log(name);
-    var name = "Kristin";
   }
 }
